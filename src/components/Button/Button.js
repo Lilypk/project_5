fd8d23863cee0a9bfd23c7919ff0f2471aada289
LILYKLOPCIC <LILYKLOPCIC@GMAIL.COM>
2020-10-22 11:16:46 -0400

    demo

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,10 +1,34 @@
 import React from 'react';
-
+import './Button.css'
 // This is a functional component - just sent up a little differently as an arrow function!
-const Button = (props) => (
-    <button>
-        {props.label}
+const Button = (props) => {
+
+    let classList = ''
+
+    let types = ['primary', 'danger', 'success', 'warning']
+
+    if(types.includes(props.type)){
+            classList+= `button-${props.type}`
+        
+    }
+    if(props.outline) {
+        classList+= `button-${props.type}-outline`
+    }
+
+    console.log(props.large)
+    if(props.large){
+        classList += ` button-large`
+    }
+    if(props.outline){
+        classList += ` button-outline`
+    }
+
+   return (
+   <button onClick={props.onClick} className={classList}>
+       {props.label}
+        
     </button>
 )
+   }
 
 export default Button; 
--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -3,5 +3,48 @@ import { storiesOf } from '@storybook/react';
 
 import Button from './Button';
 
+
+function helloWorld() {
+    alert('Button Component Executed props.onClick')
+}
+
 storiesOf('Button', module)
-    .add('Primary', () => <Button label="Primary" />) 
+    .add('Primary', () => <Button 
+        type='primary'
+        onClick={helloWorld}
+        label="Primary" />) 
+    .add('Large Primary', () => <Button
+        type='primary'
+        large
+        label='Large Primary Button'/>)
+    
+    
+    .add('Success', () => <Button 
+        type='success'
+        label="Success" />) 
+    .add('Large Success', () => <Button
+        type='success'
+        large
+        label='Large Success Button'/>)
+        
+    
+    
+    // .add('Primary', () => <Button 
+    //     type='primary'
+    //     label="Primary" />) 
+    // .add('Large Primary', () => <Button
+    // type='primary'
+    // large
+    // label='Large Primary Button'/>)
+    
+    // .add('Primary', () => <Button 
+    //     type='primary'
+    //     label="Primary" />) 
+    // .add('Large Primary', () => <Button
+    // type='primary'
+    // large
+    // label='Large Primary Button'/>)
+    
+
+
+
