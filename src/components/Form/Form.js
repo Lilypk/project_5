80b8c2d787d13c45671ff502a0d13d7370dcf1cf
LILYKLOPCIC <LILYKLOPCIC@GMAIL.COM>
2020-10-25 17:08:56 -0400

    grr froms

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,3 +1,35 @@
 import React from 'react'
 import './Form.css'
 
+const Form = (props) => {
+
+    let classList = ''
+
+    let types = ['email', 'select']
+
+    if(types.includes(props.type)){
+        classList+= ` form-${props.type}`
+    }
+    if(props.medium) {
+        classList += ` form-${props.type}-medium`
+    }
+    // if(props.outline) {
+    //     classList += ` button-${props.type}-outline`
+    // }
+    // if(props.faded) {
+    //     classList += ` button-${props.type}-faded`
+    // }
+    // if(props.large){
+    //     classList += ` button-large`
+    // }
+  
+
+   return (
+   <input  className={classList}>
+       {props.label}
+        
+    </input>
+)
+   }
+
+export default Form; 
--- src/components/Form/Form.stories.js
+++ src/components/Form/Form.stories.js
@@ -3,3 +3,22 @@ import { storiesOf } from '@storybook/react'
 import Form from './Form'
 
 storiesOf('Form', module)
+
+    .add('Email Input', () => <input
+       label='Email'
+       placeholder='Email'
+       type='email' />) 
+    .add('Email Input Medium', () => <input
+       placeholder='Email'
+       type='email'
+       medium
+       label='Email' />) 
+    .add('Email Input Large', () => <input
+        label='Email'
+        placeholder='Email'
+        type='email'
+        large />)
+    .add('Dropdown Large', () => <select 
+         buttonText='Dropdown'
+         label='select' />)
+
