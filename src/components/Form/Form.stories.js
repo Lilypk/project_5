9da01e1a6daaec5f540f2a251c1040a062194a7a
LILYKLOPCIC <LILYKLOPCIC@GMAIL.COM>
2020-10-25 17:19:38 -0400

    not done

--- src/components/Form/Form.stories.js
+++ src/components/Form/Form.stories.js
@@ -1,24 +1,19 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-import Form from './Form'
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import Form from "./Form";
 
-storiesOf('Form', module)
+storiesOf("Form", module)
+  .add("Email Input", () => (
+    <input label="Email" placeholder="Email" type="email" />
+  ))
+  .add("Email Input Medium", () => (
+    <input placeholder="Email" type="email" medium label="Email" />
+  ))
+  .add("Email Input Large", () => (
+    <input label="Email" placeholder="Email" type="email" large />
+  ))
+  .add("Dropdown Large", () => (
+    <select type="select" label="select" />
+  ))
 
-    .add('Email Input', () => <input
-       label='Email'
-       placeholder='Email'
-       type='email' />) 
-    .add('Email Input Medium', () => <input
-       placeholder='Email'
-       type='email'
-       medium
-       label='Email' />) 
-    .add('Email Input Large', () => <input
-        label='Email'
-        placeholder='Email'
-        type='email'
-        large />)
-    .add('Dropdown Large', () => <select 
-         buttonText='Dropdown'
-         label='select' />)
-
+  
